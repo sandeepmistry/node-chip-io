a9e2d8718842595580dea0acce78b8644b6877eb
Sandeep Mistry <sandeep.mistry@gmail.com>
2016-02-18 19:47:10 -0500

    Use change event for internal temperature example

--- examples/internal-temperature.js
+++ examples/internal-temperature.js
@@ -1,20 +1,16 @@
 var five = require('johnny-five');
 var ChipIO = require('../index'); // or require('chip-io');
+
 var board = new five.Board({
   io: new ChipIO()
 });
 
 board.on('ready', function() {
-  var temperature = null;
   var thermometer = new five.Thermometer({
     controller: ChipIO.Controllers.INTERNAL_TEMP
   });
-  thermometer.on('data', function() {
-    var celsius = Math.round(this.celsius);
 
-    if (temperature !== celsius) {
-      temperature = celsius;
-      console.log('%d°C', temperature);
-    }
+  thermometer.on('change', function(data) {
+    console.log('Internal temperature is ' + data.celsius.toFixed(2) + '°C');
   });
 });
