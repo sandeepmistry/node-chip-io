5850cf9ed646c5a0c7a8762f6d022be1828526bc
Sandeep Mistry <sandeep.mistry@gmail.com>
2016-07-17 16:50:23 -0400

    Update internal temperature example to use chipio.InternalTemperature

--- examples/internal-temperature.js
+++ examples/internal-temperature.js
@@ -1,14 +1,12 @@
 var five = require('johnny-five');
-var ChipIO = require('../index'); // or require('chip-io');
+var chipio = require('../index'); // or require('chip-io');
 
 var board = new five.Board({
-  io: new ChipIO()
+  io: new chipio()
 });
 
 board.on('ready', function() {
-  var thermometer = new five.Thermometer({
-    controller: ChipIO.Controllers.INTERNAL_TEMP
-  });
+  var thermometer = new chipio.InternalTemperature();
 
   thermometer.on('change', function(data) {
     console.log('Internal temperature is ' + data.celsius.toFixed(2) + '°C');
