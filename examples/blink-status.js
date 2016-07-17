b4f21e3a5477a106905d0bda9993c01cd1f6a30b
Sandeep Mistry <sandeep.mistry@gmail.com>
2016-07-17 16:50:00 -0400

    Update blink status example to use chipio.StatusLed

--- examples/blink-status.js
+++ examples/blink-status.js
@@ -1,14 +1,14 @@
 var five = require('johnny-five');
-var ChipIO = require('../index'); // or require('chip-io');
+var chipio = require('../index'); // or require('chip-io');
 
 var board = new five.Board({
-  io: new ChipIO()
+  io: new chipio()
 });
 
 board.on('ready', function() {
   // Create an LED for the STATUS LED
-  var led = new five.Led('STATUS');
+  var statusLed = new chipio.StatusLed();
 
   // Blink every half second
-  led.blink(500);
+  statusLed.blink(500);
 });
