4cfd9c1fa7f7777d160ad01baced53164a8bf6a9
Sandeep Mistry <sandeep.mistry@gmail.com>
2016-07-17 16:47:54 -0400

    Add StatusLed, BatteryVoltage, and InternalTemperature types

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var ChipIO = require('./lib/chip-io');
 
-
 ChipIO.Controllers = require('./lib/controllers');
+
+ChipIO.BatteryVoltage = require('./lib/battery-voltage');
+ChipIO.InternalTemperature = require('./lib/internal-temperature');
+ChipIO.StatusLed = require('./lib/status-led');
 
 module.exports = ChipIO;
